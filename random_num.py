de3db5539c7bd8b3f9317bd4b344600bd9755bb2
fullstack290623 <fullstack290623@gmail.com>
2026-02-01 20:42:22 +0200

    main code

---
+++ random_num.py
@@ -0,0 +1,17 @@
+
+
+# random a number between 1-100 == lucky
+# input number
+# while user did not guess the number (must be between 1-100):
+#   if the user guessed lower than lucky, print "guess higher"
+#   if the user guessed higher than lucky, print "guess lower"
+#   if he guessed the exact number print "Bingo"
+#   if the user guessed more than 7 times then exit game and print "you lost!"
+
+# 88
+# 20, 100, 85, 89, 87, 90
+
+# Higher ⬆️
+# Lower ⬇️
+# Bingo 🎯 💥
+# player loose 😢
