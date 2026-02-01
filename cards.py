504196ef1691d80a79d2ea81854ec4bb25236bec
fullstack290623 <fullstack290623@gmail.com>
2026-02-01 21:44:03 +0200

    main code

--- cards.py
+++ cards.py
@@ -1,7 +1,7 @@
 
 import random
 
-# choose suit
+# choose 10 random cards
 for _ in range(10):
     suit = random.choice(["❤️", "♦️", "♣️", "♠️"])
     card = random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'])
